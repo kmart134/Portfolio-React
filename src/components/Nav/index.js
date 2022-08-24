cadfbf8158236c28ae3acaa94d79b216ea890bda
kmart134 <ksmgps@hotmail.com>
2022-08-24 19:32:31 -0400

    index.js on nav folder

---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+function Nav(props) {
+    const {
+        pages = [],
+        setCurrentPage,
+        currentPage,
+    } = props;
+
+    return(
+        //nav
+        //map over pages to render the name of each page - activity 10 solved
+        //have an onClick for each of these elements created by the map, that sets the 
+        //current page equal to that page - using setCurrentPage that we get from App.js
+    )
+}
