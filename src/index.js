14f906f8b94c3f7817022999bb9afcf1ddeb8a10
kmart134 <ksmgps@hotmail.com>
2022-08-24 18:16:39 -0400

    index.js updated

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import './index.css';
+import App from './App';
+
+reactDom.render(
+    <React.strictMode>
+        <App/>
+    </React.strictMode>,
+    document.getElementById('root')
+);
