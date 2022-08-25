34659a48f861f9cd349938215e66b3ddb430b298
kmart134 <ksmgps@hotmail.com>
2022-08-25 19:26:44 -0400

    nav abr is rendering

--- my-portfolio/src/components/Nav/index.js
+++ my-portfolio/src/components/Nav/index.js
@@ -1,26 +1,69 @@
 import React, { useEffect } from 'react';
 
-function Nav(props) {
-    const {
-        pages = [],
-        setCurrentPage,
-        currentPage,
-    } = props;
+function Nav({currentPage, handlePageChange}) {
+    // const {
+    //     pages = [],
+    //     setCurrentPage,
+    //     currentPage,
+    // } = props;
 
     return(
         //nav
+        <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="about-me"
+          onClick={() => handlePageChange('About')}
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="contact"
+          onClick={() => handlePageChange('Contact')}
+          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="resume"
+          onClick={() => handlePageChange('Resume')}
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
         //map over pages to render the name of each page - activity 10 solved
         //have an onClick for each of these elements created by the map, that sets the 
         //current page equal to that page - using setCurrentPage that we get from App.js
-        <ul className="list-group">
-      {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
-      {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
-      {props.groceries.map(item => (
-        <li className="list-group-item" key={item.id}>
-          {item.name}
-        </li>
-      ))}
-    </ul>
+    //     <ul className="list-group">
+    //   {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
+    //   {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
+    //   {props.groceries.map(item => (
+    //     <li className="list-group-item" key={item.id}>
+    //       {item.name}
+    //     </li>
+    //   ))}
+    // </ul>
     );
 }
 
--- my-portfolio/src/components/Page/index.js
+++ my-portfolio/src/components/Page/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import PageContent from'../PageContent';
+import React , { useState } from 'react';
+// import PageContent from'../PageContent';
 import About from '../About';
 import Portfolio from '../Portfolio';
 import Contact from '../Contact';
 // import Resume from '../Resume';
-import { capitalizeFirstLetter} from '../../utils/helper';
+import Nav from "../Nav";
+// import { capitalizeFirstLetter} from '../../utils/helper';
 
-function Page({ currentPage}) {
+export default function Page()  {
+
+    const [currentPage, setCurrentPage] = useState('About');
     const renderPage = () =>{
         //conditional that returns the correct component based on our currentPage.name
         switch (currentPage.name){
@@ -25,18 +28,25 @@ function Page({ currentPage}) {
 
         //switch case, if statement
 
-        //currentPage.name is 'about me' - we wont return <About/> component
-
+        //currentPage.name is 'about me' - we wont return <About/> component  
     };
 
+    const handlePageChange = (page) => setCurrentPage(page);
+
     return(
-        <section>
-            <h2>{capitalizeFirstLetter(currentPage.name)}</h2>
-            <PageContent>{renderPage()}</PageContent>
-        </section>
+        // <section>
+        //     <h2>{capitalizeFirstLetter(currentPage.name)}</h2>
+        //     <PageContent>{renderPage()}</PageContent>
+        // </section>
+        <div>
+        {/* We are passing the currentPage from state and the function to update it */}
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* Here we are calling the renderPage method which will return a component  */}
+        {renderPage()}
+      </div>
         //container or section
         
-    )
+    );
 }
 
-export default Page
+// export default Page
