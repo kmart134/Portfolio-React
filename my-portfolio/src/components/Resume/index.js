af11176bf8c4c090f8d9951da9aa72dc7756c29e
kmart134 <ksmgps@hotmail.com>
2022-08-28 15:27:28 -0400

    resuem rendering on page

--- my-portfolio/src/components/Resume/index.js
+++ my-portfolio/src/components/Resume/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import coverImage from '../../assets/images/laptop.jpg';
+import resume from '../../assets/images/Resume.png'
 import '../../assets/Resume.css';
+
 
 function Resume(){
     return(
         <div className="content">
         <section className="about-me">
-            <img id= "headshot" src="../../assets/images/karen.jpg" alt="Karen Headshot"/>
+            <img id= "resume" src={resume} alt="Karen resume"/>
             <h2>Resume</h2>
             <p className="body-text">
             Resume should be here
