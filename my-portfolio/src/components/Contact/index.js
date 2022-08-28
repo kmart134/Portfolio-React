d5f9b10a385381fdca5116157406a3d67c054964
kmart134 <ksmgps@hotmail.com>
2022-08-28 17:18:54 -0400

    contact css added and github link added

--- my-portfolio/src/components/Contact/index.js
+++ my-portfolio/src/components/Contact/index.js
@@ -29,10 +29,17 @@ function Contact(){
     return(
         <div className="content">
         <section className="about-me">
-            <img id= "headshot" src="../../assets/images/karen.jpg" alt="Karen Headshot"/>
+            <img id= "headshot" src={require("../../assets/images/karen.jpg")} alt="Karen Headshot"/>
             <h2>Contact</h2>
             <p className="body-text">
-           Contact will be here
+            <ul>
+                    <li id="linkedin">
+                        <a href= "https://www.linkedin.com/in/kmartinezguerrero/"> Visit my LinkedIn!</a> </li>
+                    <li id="github">
+                        <a href= "https://github.com/kmart134"> Visit my GitHub!!</a> </li>
+                    <li>Email: ksmgps@hotmail.com</li>
+                    <li>Phone Number: 321-287-8935</li>
+                </ul>
             </p>
         </section>
         </div>
