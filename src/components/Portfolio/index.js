2403ff6e67d1ea2c058a7ce965d8a86497dcdb6c
kmart134 <ksmgps@hotmail.com>
2022-08-29 15:08:10 -0400

    unzipped parent folder

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,42 @@
+//instructo provided
+import React, { useState } from 'react';
+import '../../assets/Portfolio.css';
+import Project from "../Project";
+
+function Portfolio() {
+    const [projects] = useState([
+        {
+            name: 'Backyard Jungle',
+            description: 'This growing website allows plant-lovers to add plants to their gardens (collections) and interact with other others based on their interests. This was a collaboration with other students in the UCF Full-Stack coding Bootcamp.',
+            link: "https://dry-beach-49040.herokuapp.com/",
+            repo: "https://github.com/Anastasia095/Backyard-Jungle"
+        },
+        {
+            name: 'Mix-up-Movie-Night',
+            description: 'This nifty application will quickly switch up boring date nights. The application will suggest a cocktail and movie at random to get you out of your boring movie night rut. This was a collaboration with other students in the UCF Full-Stack coding Bootcamp. ',
+            link: "https://servbt.github.io/Mix-up-Movie-Night/",
+            repo: "https://github.com/Servbt/Mix-up-Movie-Night"
+        },
+        {
+            name: 'Code-Quiz',
+            description: 'Coding Quiz: Test your knowledge in the world of code. Watch out: every time you get a question wrong, time will be deducted!',
+            link: "https://servbt.github.io/Mix-up-Movie-Night/",
+            repo: "https://github.com/Servbt/Mix-up-Movie-Night"
+        },
+    ]);
+ return(
+    <div>
+        
+    <div className= "port flex-row">
+        {projects.map((project, idx) =>(
+            <Project 
+            project={project}
+            key={"project" + idx}
+            />
+        ))}
+        </div>
+    </div>
+ );
+};
+
+export default Portfolio;
